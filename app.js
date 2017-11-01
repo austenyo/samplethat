0198a8804f342695f59178fcba55bfcab9cd8da1
Fdebies <Fabien.Debies16@kzoo.edu>
2017-11-01 15:18:48 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+require("./bundle-config");
+//Object.defineProperty(exports, "__esModule", { value: true });
+var application = require("application");
+application.start({ moduleName: "main-page" });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEseUNBQTJDO0FBQzNDLFdBQVcsQ0FBQyxLQUFLLENBQUMsRUFBRSxVQUFVLEVBQUUsV0FBVyxFQUFFLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAqIGFzIGFwcGxpY2F0aW9uIGZyb20gXCJhcHBsaWNhdGlvblwiO1xuYXBwbGljYXRpb24uc3RhcnQoeyBtb2R1bGVOYW1lOiBcIm1haW4tcGFnZVwiIH0pO1xuIl19
